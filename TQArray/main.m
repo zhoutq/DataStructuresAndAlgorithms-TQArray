b461e5d34004b64b31ef26ac27d35d91972c0e95
ztq <ztq@ztqdeMac-mini.local>
2017-11-25 14:39:45 +0800

    Initial Commit

---
+++ TQArray/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TQArray
+//
+//  Created by ztq on 2017/11/25.
+//  Copyright © 2017年 ztq. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
